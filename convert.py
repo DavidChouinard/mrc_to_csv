3976d203290f147b6ea8532a6ef0ac2533812743
David Chouinard <david@davidchouinard.com>
2013-11-24 11:43:06 -0500

    Added conversion script

---
+++ convert.py
@@ -0,0 +1,63 @@
+from pymarc import MARCReader
+import csv
+
+def main():
+    filename = 'ab.bib.01.20131101.full'
+    reader = MARCReader(file('data/hlom/' + filename + '.mrc'))
+
+    with open('data/processed/' + filename + '.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(['isbn', 'title', 'author',
+                        'publisher', 'pubplace', 'pubyear',
+                        'extent', 'dimensions', 'subject', 'inclusiondate',
+                        'source', 'library', 'notes'])
+
+        for i, record in enumerate(reader):
+            #print record
+            pubplace = clean(record['260']['a']) if '260' in record else None
+            extent = clean(record['300']['a'], True) if '300' in record else None
+            dimensions = record['300']['c'] if '300' in record else None
+            subject = record['650']['a'] if '650' in record else None
+            inclusiondate = record['988']['a'] if '988' in record else None
+            source = record['906']['a'] if '906' in record else None
+            library = record['690']['5'] if '690' in record else None
+
+            notes = " ".join([field['a'] for field in record.notes() if 'a' in field])
+
+            writer.writerow([record.isbn(), get_title(record), clean(record.author(), True),
+                            clean(record.publisher()), pubplace, clean(record.pubyear()),
+                            extent, dimensions, subject, inclusiondate,
+                            source, library, notes])
+
+            if i % 100 == 0:
+                print filename + ": " + str(i) + " documents processed"
+
+def get_title(record):
+    # pymarc has a title() method that is similar to this, but it doesn't
+    # conjoin subtitle and title properly
+    if '245' in record and 'a' in record['245']:
+        title = clean(record['245']['a'])
+        if 'b' in record['245']:
+            title += ' ' + clean(record['245']['b'])
+        return title
+    else:
+        return None
+
+def clean(element, isAuthor=False):
+    if element is None:
+        return element
+    else:
+        element = element.strip()
+
+        for character in [',', ';', ':', '/']:
+            if element[-1] == character:
+                return element[:-1].strip()
+
+        if not isAuthor and element[-1] == '.':
+            # don't strip trailing periods from author names
+            return element[:-1].strip()
+
+        return element.strip()
+
+if __name__ == "__main__":
+    main()
